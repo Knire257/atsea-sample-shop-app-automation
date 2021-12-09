cfe1c58044c870d230856276295a531d6f47711b
Daniel Naranjo <danaranjor@unal.edu.co>
2021-12-08 23:19:12 -0500

    docker conection stablished, ui initial config

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -1,10 +1,30 @@
 
 import { Config, browser } from 'protractor';
 
+const firefoxConfig = {
+    browserName: 'firefox',
+    name: 'firefox-tests',
+    shardTestFiles: true,
+    maxInstances: 1,
+    'moz:firefoxOptions': {
+        args: ['--headless', '--window-size=1920,1080']
+    }
+};
+
+const chromeConfig = {
+    browserName: 'chrome',
+    name: 'chrome-tests',
+    shardTestFiles: true,
+    maxInstances: 1,
+    'chromeOptions': {
+        args: ['--headless', '--window-size=1920,1080']
+    }
+};
+
 export const config: Config = {
     framework: 'mocha',
-    specs: ['../test/ui/**/*.js'],      
-    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['../test/ui/**/*.js'],
+    seleniumAddress: 'http://0.0.0.0:4444/wd/hub',
     SELENIUM_PROMISE_MANAGER: false,
     onPrepare: () => {
         browser.ignoreSynchronization = true;
@@ -13,9 +33,7 @@ export const config: Config = {
     },
     mochaOpts: {
         reporter: 'mochawesome-screenshots',
-        timeout: 120000,
+        timeout: 60000,
     },
-    capabilities: {
-        browserName: 'chrome',
-    }
+    multiCapabilities: [chromeConfig, firefoxConfig]
 };
