2a5cc88f242565926981ee4feee2793890868f80
Daniel Naranjo <39471311+Knire257@users.noreply.github.com>
2021-12-13 20:39:17 -0500

    UI tests updated to EC2

--- test/ui/accessShop.spec.ts
+++ test/ui/accessShop.spec.ts
@@ -8,7 +8,7 @@ describe('First test', () => {
   browser.manage().timeouts().implicitlyWait(0)
   describe('When opening th store page', () => {
     it('then should have a title', async () => {
-      await browser.get('http://host.docker.internal:8080');
+      await browser.get('http://ec2-3-144-243-225.us-east-2.compute.amazonaws.com:8080/');
       await browser.wait(expectedConditions.presenceOf(mainPage.getPageTitle()), 5000);
       expect(await browser.getTitle()).to.equal('Atsea Shop')
     })
