5942e667bf3bf0050e95f57334987acfbf7403aa
Daniel Naranjo <39471311+Knire257@users.noreply.github.com>
2021-12-15 18:36:44 -0500

    Update systemUtilities-test.ts

--- test/api/systemUtilities-test.ts
+++ test/api/systemUtilities-test.ts
@@ -4,7 +4,7 @@ describe("System utilities Tests", () => {
   const chai1 = require('chai');
   const expect = chai1.expect;
   it("Database healtcheck", async () => {
-    const response = await agent.get("http://ec2-3-144-243-225.us-east-2.compute.amazonaws.com:8080/utility/healthcheck/")
+    const response = await agent.get("http://ec2-13-58-210-183.us-east-2.compute.amazonaws.com:8080/utility/healthcheck/")
       .set("Content-type", "application/json")
       .set("Accept", "application/json");
     expect(response.status).to.equal(StatusCodes.OK);
@@ -12,7 +12,7 @@ describe("System utilities Tests", () => {
   });
 
   it("Getting container ID using GET", async () => {
-    const response = await agent.get("http://ec2-3-144-243-225.us-east-2.compute.amazonaws.com:8080/utility/containerid/")
+    const response = await agent.get("http://ec2-13-58-210-183.us-east-2.compute.amazonaws.com:8080/utility/containerid/")
       .set("Content-type", "application/json")
       .set("Accept", "application/json");
     expect(response.status).to.equal(StatusCodes.OK);
